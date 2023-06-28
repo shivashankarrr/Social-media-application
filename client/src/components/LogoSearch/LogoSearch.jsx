235984946c95c8d8acc326ab38da2248519d1f77
shiva shankar <shankuu786@gmail.com>
2023-06-28 08:58:06 +0530

    first commit

---
+++ client/src/components/LogoSearch/LogoSearch.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo from "../../img/logo.png";
+import './LogoSearch.css'
+import { UilSearch } from '@iconscout/react-unicons'
+import { Link } from "react-router-dom";
+const LogoSearch = () => {
+  return (
+    <div className="LogoSearch">
+      <Link to="../home">
+        <img src={Logo} alt="" />
+      </Link>
+      <div className="Search">
+          <input type="text" placeholder="#Explore"/>
+          <div className="s-icon">
+          <Link to="../home">
+                <UilSearch/>
+          </Link>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogoSearch;
---
+++ client/src/components/RightSide/RightSide.jsx
@@ -0,0 +1,20 @@
+import "./RightSide.css";
+import TrendCard from "../TrendCard/TrendCard";
+import NavIcons from "../NavIcons/NavIcons";
+const RightSide = () => {
+
+  return (
+    <div className="RightSide">
+      {/* Side Navbar */}
+
+      <NavIcons />
+      {/* TrendCard */}
+      <TrendCard />
+
+      {/* Share buttong */}
+
+    </div>
+  );
+};
+
+export default RightSide;
