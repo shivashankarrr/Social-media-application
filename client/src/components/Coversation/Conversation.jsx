50e3a6fc8d5fa594aff569173fc4a8fcf96b6df4
shiva <shankuu786@gmail.com>
2024-02-11 15:58:05 +0530

    first commit

--- client/src/components/Coversation/Conversation.jsx
+++ client/src/components/Coversation/Conversation.jsx
@@ -31,19 +31,26 @@ const Conversation = ({ data, currentUser, online }) => {
         <div>
           {online && <div className="online-dot"></div>}
           <img
-            src={userData?.profilePicture? process.env.REACT_APP_PUBLIC_FOLDER + userData.profilePicture : process.env.REACT_APP_PUBLIC_FOLDER + "defaultProfile.png"}
+            src={
+              userData?.profilePicture
+                ? process.env.REACT_APP_PUBLIC_FOLDER + userData.profilePicture
+                : process.env.REACT_APP_PUBLIC_FOLDER + "defaultProfile.png"
+            }
             alt="Profile"
             className="followerImage"
             style={{ width: "50px", height: "50px" }}
           />
-          <div className="name" style={{fontSize: '0.8rem'}}>
-            <span>{userData?.firstname} {userData?.lastname}</span>
+          <div className="name" style={{ fontSize: "0.8rem" }}>
+            <span>
+              {userData?.firstname} {userData?.lastname}
+            </span>
           </div>
         </div>
       </div>
       <hr style={{ width: "85%", border: "0.1px solid #ececec" }} />
     </>
   );
+  
 };
 
 export default Conversation;
